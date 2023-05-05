7c6179814b0322c604303532284f022869e7422d
Francois Laubscher <francoisjlaubscher@gmail.com>
2023-05-05 20:13:06 +0200

    fix: add esc listener and classname to modal

--- src/components/modal/index.tsx
+++ src/components/modal/index.tsx
@@ -1,18 +1,38 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import classnames from 'classnames';
 
 import styles from './modal.module.scss';
 
 export interface Props {
   children: React.ReactNode;
+  className?: string;
   visible: boolean;
+  onClose?: () => void;
   onOverlayClick?: React.MouseEventHandler<HTMLDivElement> | undefined;
 }
 
-const Modal = ({ children, visible, onOverlayClick }: Props) => (
-  <div className={classnames(styles.modal, visible && styles.visible)} onClick={onOverlayClick}>
-    <div className={styles.content}>{children}</div>
-  </div>
-);
+const Modal = ({ children, className, visible, onClose, onOverlayClick }: Props) => {
+  useEffect(() => {
+    if (onClose) {
+      const listener = (key: KeyboardEvent) => {
+        if (key.key === 'Escape') {
+          onClose();
+        }
+      };
+
+      document.addEventListener('keyup', listener);
+
+      return () => {
+        document.removeEventListener('keyup', listener);
+      };
+    }
+  }, []);
+
+  return (
+    <div className={classnames(styles.modal, visible && styles.visible)} onClick={onOverlayClick}>
+      <div className={classnames(styles.content, className)}>{children}</div>
+    </div>
+  );
+};
 
 export default Modal;
