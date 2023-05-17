caaebf6a7dccbe6b90e98a09bb07207bd1bf0d6d
Francois Laubscher <francoisjlaubscher@gmail.com>
2023-05-17 11:37:49 +0200

    fix: allow args for useAsync

--- src/hooks/use-async.ts
+++ src/hooks/use-async.ts
@@ -2,7 +2,7 @@
 import { useCallback, useEffect, useState } from 'react';
 
 const useAsync = <T, E = string>(
-  asyncFunction: () => Promise<T>,
+  asyncFunction: (...args: any[]) => Promise<T>,
   deps?: React.DependencyList,
   immediate = true
 ) => {
@@ -14,20 +14,23 @@ const useAsync = <T, E = string>(
   // handles setting state for pending, value, and error.
   // useCallback ensures the below useEffect is not called
   // on every render, but only if asyncFunction changes.
-  const execute = useCallback(() => {
-    setStatus('pending');
-    setValue(null);
-    setError(null);
-    return asyncFunction()
-      .then((response: any) => {
-        setValue(response);
-        setStatus('success');
-      })
-      .catch((error: any) => {
-        setError(error);
-        setStatus('error');
-      });
-  }, [asyncFunction, deps]);
+  const execute = useCallback(
+    (...args: any[]) => {
+      setStatus('pending');
+      setValue(null);
+      setError(null);
+      return asyncFunction(...args)
+        .then((response: any) => {
+          setValue(response);
+          setStatus('success');
+        })
+        .catch((error: any) => {
+          setError(error);
+          setStatus('error');
+        });
+    },
+    [asyncFunction, deps]
+  );
 
   // Call execute if we want to fire it right away.
   // Otherwise execute can be called later, such as
