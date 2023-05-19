39316a9b367691d4a34b6ecfa6485a3af3c7f168
Francois Laubscher <francoisjlaubscher@gmail.com>
2023-05-19 22:05:35 +0200

    feature: add search to layout

--- src/components/layout/layout.stories.tsx
+++ src/components/layout/layout.stories.tsx
@@ -5,6 +5,7 @@ import type { StoryObj, Meta } from '@storybook/react';
 import Button from '../button';
 import IconButton from '../button/icon';
 import Layout from '.';
+import InputField from '../field/input';
 
 const meta: Meta<typeof Image> = {
   title: 'Layout',
@@ -37,6 +38,32 @@ export const WithMenu: StoryObj<typeof Layout> = {
       </IconButton>
     ),
     title: 'Page Title',
+    action: (
+      <IconButton>
+        <FaPlus />
+      </IconButton>
+    ),
+    children:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit corrupti delectus fuga minus odit fugit voluptatum corporis illo veniam, eveniet atque, saepe sint architecto nostrum est? Aliquam sequi placeat magni.',
+    menu: (
+      <>
+        <Button>Menu Item 1</Button>
+        <Button>Menu Item 2</Button>
+        <Button>Menu Item 3</Button>
+      </>
+    )
+  }
+};
+
+export const WithSearch: StoryObj<typeof Layout> = {
+  args: {
+    home: (
+      <IconButton>
+        <FaHome />
+      </IconButton>
+    ),
+    title: 'Page Title',
+    onSearchChange: (term) => Promise.resolve(console.log(term)),
     action: (
       <IconButton>
         <FaPlus />
