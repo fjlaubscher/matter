144a95c08584346982d0f2965be8ec5a26ec72ab
Francois Laubscher <francoisjlaubscher@gmail.com>
2024-05-17 14:28:48 +0200

    feature: add onClick handlers to Stat component

--- src/components/Stat/index.tsx
+++ src/components/Stat/index.tsx
@@ -7,13 +7,29 @@ export type Props = {
   description?: string;
   title: string;
   value: string | number;
+  titleOnClick?: () => void;
+  descriptionOnClick?: () => void;
 } & React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>;
 
-const Stat = ({ className, description, title, value, ...rest }: Props) => (
+const Stat = ({
+  className,
+  description,
+  title,
+  value,
+  titleOnClick,
+  descriptionOnClick,
+  ...rest
+}: Props) => (
   <div className={classnames(styles.stat, className)} {...rest}>
-    <span className={styles.title}>{title}</span>
+    <span className={styles.title} onClick={titleOnClick}>
+      {title}
+    </span>
     <span className={styles.value}>{value}</span>
-    {description && <span className={styles.description}>{description}</span>}
+    {description && (
+      <span className={styles.description} onClick={descriptionOnClick}>
+        {description}
+      </span>
+    )}
   </div>
 );
 
--- src/helpers/text.ts
+++ src/helpers/text.ts
@@ -1,3 +1,21 @@
 export const slugify = (input: string) => input.toLowerCase().replace(/ /g, '-');
 
 export const capitalize = (input: string) => input[0].toUpperCase() + input.slice(1);
+
+export const snakeCaseToCamelCase = (input: string) =>
+  input.replace(/_([a-z])/g, (_, char) => char.toUpperCase());
+
+export const convertSnakeCaseObjectToCamelCase = (obj: { [key: string]: unknown }) => {
+  if (typeof obj !== 'object' || obj === null) {
+    return obj;
+  }
+
+  const newObj: { [key: string]: unknown } = {};
+  Object.keys(obj).forEach((key) => {
+    const newKey = snakeCaseToCamelCase(key);
+    // call recursively for nested objects
+    newObj[newKey] = convertSnakeCaseObjectToCamelCase(obj[key] as { [key: string]: unknown });
+  });
+
+  return newObj;
+};
